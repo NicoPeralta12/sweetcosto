5b397198c1b1096f1ede71cd2f4b1d43ac45626a
Nico Peralta <pnicolas226@gmail.com>
2024-11-05 21:53:53 -0300

    Cambio link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadFile() {
     const link = document.createElement('a');
-    link.href = 'https://drive.google.com/uc?export=download&id=18-fPvXDbAZcavv6auW3TRKcFlgThASe3';  // Reemplaza con la ruta de tu archivo setup
+    link.href = 'https://drive.google.com/uc?export=download&id=1vUrXSFBKbKlrDg3E5v65vOtskPgc_efQ';  // Reemplaza con la ruta de tu archivo setup
     link.download = 'setup.exe';  // Nombre del archivo que se descargará
     document.body.appendChild(link);
     link.click();
